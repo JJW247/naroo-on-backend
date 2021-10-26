419850897a7cdcd936a0f8b3d994c8829faf2ee2
jjw247 <adr10won@gmail.com>
2021-10-27 00:53:12 +0900

    env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,11 +19,7 @@ import { ResourcesModule } from './resources/resources.module';
             extra: { ssl: { rejectUnauthorized: false } },
           }
         : {
-            host: process.env.DATABASE_HOST,
-            port: +process.env.DATABASE_PORT,
-            username: process.env.DATABASE_USERNAME,
-            password: process.env.DATABASE_PASSWORD,
-            database: process.env.DATABASE_DATABASE,
+            url: process.env.DATABASE_URL,
           }),
       autoLoadEntities: true,
       synchronize: true,
